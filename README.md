b6a5d03dce9dc1aac3270ef734492b363e9c531b
Sergey Kirillov <s_kirillov@rambler.ru>
2021-05-28 12:17:10 +0300

    imported bootstrap && created README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fifteen
+The game of 15, tags, is a popular puzzle game invented in 1878 by Noah Chapman.  
+![image](480px-15.png)  
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
